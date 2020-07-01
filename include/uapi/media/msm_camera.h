7276590f225b62e6b83ce56bdc12ef56cc59e57b
dianlujitao <dianlujitao@lineageos.org>
2020-07-01 20:27:45 +0800

    Revert "msm: camera: Send topology and stream status to TZ"
    This reverts commit b1beed57e76a802aa43d9a651d3227bdf7b14068.
    
    Change-Id: I4301e8930d41e2d80d61f2a22ad2c123478b49a5

--- include/uapi/media/msm_camera.h
+++ include/uapi/media/msm_camera.h
@@ -1372,7 +1372,6 @@ struct msm_camera_csid_params {
 	uint8_t lane_cnt;
 	uint16_t lane_assign;
 	uint8_t phy_sel;
-	uint32_t topology;
 	struct msm_camera_csid_lut_params lut_params;
 };
 
@@ -1417,8 +1416,6 @@ struct csic_cfg_data {
 enum csid_cfg_type_t {
 	CSID_INIT,
 	CSID_CFG,
-	CSID_SECCAM_TOPOLOGY,
-	CSID_SECCAM_RESET,
 };
 
 struct csid_cfg_data {
