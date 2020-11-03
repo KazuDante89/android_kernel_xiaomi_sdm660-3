f614f738c52322adc9158e327347d318717229ad
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2020-11-03 19:32:00 +0530

    drivers: fbdev: msm: Update wakeup_source APIs for 4.19
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -126,7 +126,7 @@ static void mdss_fb_set_mdp_sync_pt_threshold(struct msm_fb_data_type *mfd,
 static struct fb_info *prim_fbi;
 static struct delayed_work prim_panel_work;
 static atomic_t prim_panel_is_on;
-static struct wakeup_source prim_panel_wakelock;
+static struct wakeup_source *prim_panel_wakelock;
 static void prim_panel_off_delayed_work(struct work_struct *work)
 {
 #ifdef CONFIG_FRAMEBUFFER_CONSOLE
@@ -142,7 +142,7 @@ static void prim_panel_off_delayed_work(struct work_struct *work)
 	if (atomic_read(&prim_panel_is_on)) {
 		fb_blank(prim_fbi, FB_BLANK_POWERDOWN);
 		atomic_set(&prim_panel_is_on, false);
-		__pm_relax(&prim_panel_wakelock);
+		__pm_relax(prim_panel_wakelock);
 	}
 
 	unlock_fb_info(prim_fbi);
@@ -1532,7 +1532,7 @@ static int mdss_fb_remove(struct platform_device *pdev)
 	if (mfd->panel_info && mfd->panel_info->is_prim_panel) {
 		atomic_set(&prim_panel_is_on, false);
 		cancel_delayed_work_sync(&prim_panel_work);
-		wakeup_source_trash(&prim_panel_wakelock);
+		wakeup_source_unregister(prim_panel_wakelock);
 	}
 #endif
 	mdss_fb_remove_sysfs(mfd);
@@ -2235,7 +2235,7 @@ static int mdss_fb_blank(int blank_mode, struct fb_info *info)
 	if ((info == prim_fbi) && (blank_mode == FB_BLANK_UNBLANK) &&
 		atomic_read(&prim_panel_is_on)) {
 		atomic_set(&prim_panel_is_on, false);
-		__pm_relax(&prim_panel_wakelock);
+		__pm_relax(prim_panel_wakelock);
 		cancel_delayed_work_sync(&prim_panel_work);
 		return 0;
 	}
@@ -2914,7 +2914,7 @@ static int mdss_fb_register(struct msm_fb_data_type *mfd)
 		prim_fbi = fbi;
 		atomic_set(&prim_panel_is_on, false);
 		INIT_DELAYED_WORK(&prim_panel_work, prim_panel_off_delayed_work);
-		wakeup_source_init(&prim_panel_wakelock, "prim_panel_wakelock");
+		prim_panel_wakelock = wakeup_source_register(NULL, "prim_panel_wakelock");
 	}
 #endif
 
@@ -5537,16 +5537,16 @@ int mdss_prim_panel_fb_unblank(int timeout)
 #endif
 			return 0;
 		}
-		__pm_stay_awake(&prim_panel_wakelock);
+		__pm_stay_awake(prim_panel_wakelock);
 		ret = fb_blank(prim_fbi, FB_BLANK_UNBLANK);
 		if (!ret) {
 			atomic_set(&prim_panel_is_on, true);
 			if (timeout > 0)
 				schedule_delayed_work(&prim_panel_work, msecs_to_jiffies(timeout));
 			else
-				__pm_relax(&prim_panel_wakelock);
+				__pm_relax(prim_panel_wakelock);
 		} else
-			__pm_relax(&prim_panel_wakelock);
+			__pm_relax(prim_panel_wakelock);
 		unlock_fb_info(prim_fbi);
 #ifdef CONFIG_FRAMEBUFFER_CONSOLE
 		console_unlock();
