f054ecb752b3346976959649f57fc0decf7183b8
dopaemon <polarisdp@gmail.com>
2020-11-11 01:23:07 +0700

    Revert "selinux: reduce calls to context_struct_to_string()"
    This reverts commit 8349a74fd3b80955a9da0a9f0c848297e68b0278.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -438,7 +438,6 @@ mls_ops:
 	return s[0];
 }
 
-#ifdef CONFIG_AUDIT
 /*
  * security_dump_masked_av - dumps masked permissions during
  * security_compute_av due to RBAC, MLS/Constraint and Type bounds.
@@ -529,7 +528,6 @@ out:
 
 	return;
 }
-#endif
 
 /*
  * security_boundary_permission - drops violated permissions
@@ -584,11 +582,9 @@ static void type_attribute_bounds_av(struct policydb *policydb,
 	/* mask violated permissions */
 	avd->allowed &= ~masked;
 
-#ifdef CONFIG_AUDIT
 	/* audit masked permissions */
 	security_dump_masked_av(policydb, scontext, tcontext,
 				tclass, masked, "bounds");
-#endif
 }
 
 /*
@@ -729,13 +725,13 @@ static void context_struct_compute_av(struct policydb *policydb,
 				 tclass, avd);
 }
 
-static inline int security_validtrans_handle_fail(struct selinux_state *state,
+static int security_validtrans_handle_fail(struct selinux_state *state,
 					   struct context *ocontext,
 					   struct context *ncontext,
 					   struct context *tcontext,
 					   u16 tclass)
 {
-#ifdef CONFIG_AUDIT
+	struct policydb *p = &state->ss->policydb;
 	char *o = NULL, *n = NULL, *t = NULL;
 	u32 olen, nlen, tlen;
 
@@ -753,7 +749,6 @@ out:
 	kfree(o);
 	kfree(n);
 	kfree(t);
-#endif
 
 	if (!enforcing_enabled(state))
 		return 0;
@@ -923,7 +918,6 @@ int security_bounded_transition(struct selinux_state *state,
 		index = type->bounds;
 	}
 
-#ifdef CONFIG_AUDIT
 	if (rc) {
 		char *old_name = NULL;
 		char *new_name = NULL;
@@ -943,7 +937,6 @@ int security_bounded_transition(struct selinux_state *state,
 		kfree(new_name);
 		kfree(old_name);
 	}
-#endif
 out:
 	read_unlock(&state->ss->policy_rwlock);
 
@@ -1617,14 +1610,14 @@ int security_context_to_sid_force(struct selinux_state *state,
 					    sid, SECSID_NULL, GFP_KERNEL, 1);
 }
 
-static inline int compute_sid_handle_invalid_context(
+static int compute_sid_handle_invalid_context(
 	struct selinux_state *state,
 	struct context *scontext,
 	struct context *tcontext,
 	u16 tclass,
 	struct context *newcontext)
 {
-#ifdef CONFIG_AUDIT
+	struct policydb *policydb = &state->ss->policydb;
 	char *s = NULL, *t = NULL, *n = NULL;
 	u32 slen, tlen, nlen;
 
@@ -1644,9 +1637,7 @@ out:
 	kfree(s);
 	kfree(t);
 	kfree(n);
-#endif
-
-	if (!selinux_enforcing)
+	if (!enforcing_enabled(state))
 		return 0;
 	return -EACCES;
 }
@@ -1940,20 +1931,18 @@ static inline int convert_context_handle_invalid_context(
 	struct selinux_state *state,
 	struct context *context)
 {
-#ifdef CONFIG_AUDIT
+	struct policydb *policydb = &state->ss->policydb;
 	char *s;
 	u32 len;
-#endif
 
 	if (enforcing_enabled(state))
 		return -EINVAL;
 
-#ifdef CONFIG_AUDIT
-	if (!context_struct_to_string(context, &s, &len)) {
-		printk(KERN_WARNING "SELinux:  Context %s would be invalid if enforcing\n", s);
+	if (!context_struct_to_string(policydb, context, &s, &len)) {
+		pr_warn("SELinux:  Context %s would be invalid if enforcing\n",
+			s);
 		kfree(s);
 	}
-#endif
 	return 0;
 }
 
@@ -1979,13 +1968,8 @@ static int convert_context(struct context *oldc, struct context *newc, void *p)
 	struct type_datum *typdatum;
 	struct user_datum *usrdatum;
 	char *s;
-	int rc = 0;
-#ifdef CONFIG_AUDIT
 	u32 len;
-#endif
-
-	if (key <= SECINITSID_NUM)
-		goto out;
+	int rc;
 
 	args = p;
 
@@ -2091,22 +2075,17 @@ static int convert_context(struct context *oldc, struct context *newc, void *p)
 
 	return 0;
 bad:
-#ifdef CONFIG_AUDIT
 	/* Map old representation to string and save it. */
 	rc = context_struct_to_string(args->oldp, oldc, &s, &len);
 	if (rc)
 		return rc;
-	context_destroy(&oldc);
-	context_destroy(c);
-	c->str = s;
-	c->len = len;
-	printk(KERN_INFO "SELinux:  Context %s became invalid (unmapped).\n",
-	       c->str);
-	rc = 0;
-	goto out;
-#else
+	context_destroy(newc);
+	newc->str = s;
+	newc->len = len;
+	newc->hash = context_compute_hash(s);
+	pr_info("SELinux:  Context %s became invalid (unmapped).\n",
+		newc->str);
 	return 0;
-#endif
 }
 
 static void security_load_policycaps(struct selinux_state *state)
@@ -3022,11 +3001,9 @@ int security_sid_mls_copy(struct selinux_state *state,
 	struct context *context1;
 	struct context *context2;
 	struct context newcon;
-	int rc;
-#ifdef CONFIG_AUDIT
 	char *s;
 	u32 len;
-#endif
+	int rc;
 
 	rc = 0;
 	if (!state->initialized || !policydb->mls_enabled) {
@@ -3065,15 +3042,14 @@ int security_sid_mls_copy(struct selinux_state *state,
 	if (!policydb_context_isvalid(policydb, &newcon)) {
 		rc = convert_context_handle_invalid_context(state, &newcon);
 		if (rc) {
-#ifdef CONFIG_AUDIT
-			if (!context_struct_to_string(&newcon, &s, &len)) {
+			if (!context_struct_to_string(policydb, &newcon, &s,
+						      &len)) {
 				audit_log(current->audit_context,
 					  GFP_ATOMIC, AUDIT_SELINUX_ERR,
 					  "op=security_sid_mls_copy "
 					  "invalid_context=%s", s);
 				kfree(s);
 			}
-#endif
 			goto out_unlock;
 		}
 	}
